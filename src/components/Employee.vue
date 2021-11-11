f1c7a08d81e45debb3f704564cb06db8fb8d6f39
Mason Hsieh <benius@gmail.com>
2021-11-11 16:58:09 +0800

    Add Employee

---
+++ src/components/Employee.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <table>
+      <thead>{{ employee.firstName }} {{ employee.lastName }}</thead>
+      <tr>
+        <td>ID</td>
+        <td>{{ employee.employeeId }}</td>
+      </tr>
+      <tr>
+        <td>Frist Name</td>
+        <td>{{ employee.firstName }}</td>
+      </tr>
+      <tr>
+        <td>Last Name</td>
+        <td>{{ employee.lastName }}</td>
+      </tr>
+      <tr>
+        <td>Birthday</td>
+        <td>{{ employee.birthday }}</td>
+      </tr>
+      <tr>
+        <td>Country</td>
+        <td>{{ employee.country }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import { defineComponent, reactive } from 'vue'
+
+export default defineComponent({
+  name: 'EmployeeDetail',
+  setup () {
+    const employee = reactive({
+      employeeId: '19911001',
+      firstName: 'John',
+      lastName: 'Chen',
+      birthday: '1970-10-05',
+      country: 'TW'
+    })
+    return { employee }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+table, th, td {
+  border: 1px solid black;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <EmployeeDetail />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import EmployeeDetail from '@/components/Employee.vue' // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
   components: {
+    EmployeeDetail,
     HelloWorld
   }
 })
